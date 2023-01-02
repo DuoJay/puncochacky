962fd88cd7dd408c689cb3447cc5166b0c586c6e
DuoJay <82710377+DuoJay@users.noreply.github.com>
2023-01-02 21:42:41 +0100

    add adults category, doesnt look nice yet

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import socksPink from '../assets/images/socks-pink.jpg';
 import stockingsBlue from '../assets/images/stockings-blue.jpg';
+import adults from '../assets/images/adults.jpg';
 
 function Products() {
   return (
@@ -12,6 +13,10 @@ function Products() {
       <Link className="category" to={'/products/stockings'}>
         <img className="category__image" src={stockingsBlue} alt="punčošky" />
         <span className="category__name">Punčošky</span>
+      </Link>
+      <Link className="category" to={'/products/adults'}>
+        <img className="category__image" src={adults} alt="pro dospělé" />
+        <span className="category__name">Pro dospělé</span>
       </Link>
     </div>
   );
