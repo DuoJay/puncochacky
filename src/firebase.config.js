323b844455557233904d1849408dddd4b305fd33
DuoJay <82710377+DuoJay@users.noreply.github.com>
2023-01-05 12:11:31 +0100

    experimeting with the config for firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+export const db = getFirestore();
