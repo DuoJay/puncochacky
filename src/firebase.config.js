8e28ca79e1570345fc6f4b23bbed575009e62a1d
DuoJay <82710377+DuoJay@users.noreply.github.com>
2023-01-05 12:13:29 +0100

    not working

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,6 +5,14 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCj4m9ywNWGXlbfnJ2zGQk3g-LE2uUXGT4',
+  authDomain: 'puncochacky-1ff94.firebaseapp.com',
+  projectId: 'puncochacky-1ff94',
+  storageBucket: 'puncochacky-1ff94.appspot.com',
+  messagingSenderId: '665328163668',
+  appId: '1:665328163668:web:3fab5593d060dc92ac06c1',
+};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
