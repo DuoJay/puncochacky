9705ff123e55941a4737169bf11239b8480cda30
Jakub Juha <jakub.juha@bootiq.io>
2023-01-24 18:27:06 +0100

    change category images to webp

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,17 +14,23 @@ function Category() {
   return (
     <div className="category-container">
       <Link className="category" to={'/category/socks'}>
-        <div className="category__image-socks"></div>
+        <div className="category__image-socks" title="kategorie ponožky"></div>
         <span className="category__name">ponožky</span>
         <span className="category__price">25,- za pár</span>
       </Link>
       <Link className="category" to={'/category/stockings'}>
-        <div className="category__image-stockings"></div>
+        <div
+          className="category__image-stockings"
+          title="kategorie punčošky"
+        ></div>
         <span className="category__name">punčošky</span>
         <span className="category__price">65,- za kus</span>
       </Link>
       <Link className="category" to={'/category/adults'}>
-        <div className="category__image-adults"></div>
+        <div
+          className="category__image-adults"
+          title="kategorie pro dospělé"
+        ></div>
         <span className="category__name" id="category-adults">
           pro dospělé
         </span>
