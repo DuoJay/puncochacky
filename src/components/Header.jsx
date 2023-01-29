7a89e5cd6a31c7f20782d1bd3c37772abd1b1648
Jakub Juha <jakub.juha@bootiq.io>
2023-01-29 16:03:56 +0100

    convert logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { Link } from 'react-router-dom';
 import useAdminAuth from '../hooks/useAdminAuth';
 
-import logo from '../assets/logo/logo.png';
+import logo from '../assets/logo/logo.webp';
 
 function Header() {
   const isAdmin = useAdminAuth();
@@ -43,7 +43,12 @@ function Header() {
   return (
     <header className="header">
       <Link className="header__logo" to={'/'}>
-        <img src={logo} alt="" className="header__logo-img" />
+        <img
+          src={logo}
+          alt="logo punčocháčky"
+          aria-label="logo punčocháčky"
+          className="header__logo-img"
+        />
       </Link>
       {isAdmin && headerAdminButtons}
     </header>
