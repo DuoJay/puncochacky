601457cc9f307ab3acfa56f24cfef59930afb98c
Jakub Juha <jakub.juha@bootiq.io>
2023-01-29 15:53:17 +0100

    convert img on home page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -14,17 +14,32 @@ function Category() {
   return (
     <div className="category-container">
       <Link className="category" to={'/category/socks'}>
-        <div className="category__image-socks" title="ponožky"></div>
+        <div
+          className="category__image-socks"
+          role="img"
+          aria-label="kategorie ponožky"
+          title="kategorie ponožky"
+        ></div>
         <span className="category__name">ponožky</span>
         <span className="category__price">25,- za pár</span>
       </Link>
       <Link className="category" to={'/category/stockings'}>
-        <div className="category__image-stockings" title="punčošky"></div>
+        <div
+          className="category__image-stockings"
+          role="img"
+          aria-label="kategorie punčošky"
+          title="kategorie punčošky"
+        ></div>
         <span className="category__name">punčošky</span>
         <span className="category__price">65,- za kus</span>
       </Link>
       <Link className="category" to={'/category/adults'}>
-        <div className="category__image-adults" title="pro dospělé"></div>
+        <div
+          className="category__image-adults"
+          role="img"
+          aria-label="kategorie pro dospělé"
+          title="kategorie pro dospělé"
+        ></div>
         <span className="category__name" id="category-adults">
           pro dospělé
         </span>
